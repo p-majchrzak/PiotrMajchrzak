96ac4bb984dc6fcba20b22a328a0b2471abf0518
p-majchrzak <piotr-majchrzak@outlook.com>
2024-10-28 12:21:27 +0100

    Dodano klasy do aplikacji.

---
+++ Konsola/Piosenka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konsola
+{
+    public class Piosenka
+    {
+        public string Artysta { get; set; }
+        public string Album { get; set; }
+        public int piosenkaNumer { get; set; }
+        public int Rok { get; set; }
+        public int numerPobran { get; set; }
+        public Piosenka(string artysta, string album, int piosenkaNumer, int rok, int numerPobran)
+        {
+            Artysta = artysta;
+            Album = album;
+            this.piosenkaNumer = piosenkaNumer;
+            Rok = rok;
+            this.numerPobran = numerPobran;
+        }
+    }
+}
---
+++ Konsola/Plik.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konsola
+{
+    public static class Plik
+    {
+        public static List<Piosenka> PobierzPlik(string sciezka)
+        {
+            List <Piosenka> listaPiosenek = new List<Piosenka>();
+            StreamReader czytaj = new StreamReader(sciezka);
+            while(!czytaj.EndOfStream)
+            {
+                string artysta = czytaj.ReadLine();
+                string album = czytaj.ReadLine();
+                int piosenkaNumer = int.Parse(czytaj.ReadLine());
+                int rok = int.Parse(czytaj.ReadLine());
+                int numerPobran = int.Parse(czytaj.ReadLine());
+                string pominLinie = czytaj.ReadLine();
+                Piosenka piosenka = new Piosenka(artysta,album,piosenkaNumer,rok,numerPobran);
+                listaPiosenek.Add(piosenka);
+            }
+            czytaj.Close();
+            return listaPiosenek;
+        }
+        public static void Wypisz(List<Piosenka> listaPiosenek)
+        {
+            foreach(Piosenka piosenka in listaPiosenek)
+            {
+                Console.WriteLine($"{piosenka.Artysta}\n{piosenka.Album}\n{piosenka.piosenkaNumer}\n{piosenka.Rok}\n{piosenka.numerPobran}");
+            }
+        }
+    }
+}
