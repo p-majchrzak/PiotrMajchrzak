21f5a56b55006a0f4c47012a52b01262ae46a7e5
p-majchrzak <piotr-majchrzak@outlook.com>
2024-10-28 12:39:55 +0100

    Dodano testy do aplikacji.

--- Konsola.Test/PlikTest.cs
+++ Konsola.Test/PlikTest.cs
@@ -3,9 +3,32 @@ namespace Konsola.Test
     public class PlikTest
     {
         [Fact]
-        public void Test1()
+        public void DlaMetodyPobierzPlik_KiedyDaneSczytaneZPliku_WtedyZgodnoscPorownywanychDanych()
         {
+            List<Piosenka> listaPoprawnych = new List<Piosenka>();
+            Piosenka poprawneDane1 = new Piosenka("Sprawdzian", "\"5TP1\"", 11, 2018, 11000102);
+            listaPoprawnych.Add(poprawneDane1); 
+            Piosenka poprawneDane2 = new Piosenka("Piotr", "\"Majchrzak\"", 11, 2018, 11000102);
+            listaPoprawnych.Add(poprawneDane2);
 
+            List<Piosenka> listaPiosenek = Plik.PobierzPlik("PlikTestowy.txt");
+            bool weryfikacja = false;
+            
+            for(int i = 0; i<listaPiosenek.Count; i++)
+            {
+                if (listaPiosenek[i].Artysta == listaPoprawnych[i].Artysta && listaPiosenek[i].Album == listaPoprawnych[i].Album
+                && listaPiosenek[i].numerPobran == listaPoprawnych[i].numerPobran && listaPiosenek[i].Rok == listaPoprawnych[i].Rok
+                && listaPiosenek[i].piosenkaNumer == listaPoprawnych[i].piosenkaNumer)
+                {
+                    weryfikacja = true;
+                }
+                else
+                {
+                    weryfikacja = false;
+                    break;
+                }
+            }
+            Assert.True(weryfikacja);
         }
     }
 }
